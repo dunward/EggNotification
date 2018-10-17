3fce7a2399fc8dbafe2fd762c3fe753275b0b28e
eglik <eglik@odin.vision>
2018-10-17 11:22:04 +0900

    feat: Image and Textblock

--- CustomNotification/CustomNotification/MainWindow.xaml.cs
+++ CustomNotification/CustomNotification/MainWindow.xaml.cs
@@ -15,17 +15,7 @@ namespace CustomNotification
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            NotificationWindow notificationWindow = new NotificationWindow();
-
-            notificationWindow.Width = SystemParameters.PrimaryScreenWidth / 4;
-            notificationWindow.Height = SystemParameters.PrimaryScreenHeight / 8;
-
-            notificationWindow.Left = SystemParameters.PrimaryScreenWidth - (SystemParameters.PrimaryScreenWidth / 4) - 20;
-            notificationWindow.Top = SystemParameters.PrimaryScreenHeight - (SystemParameters.PrimaryScreenHeight / 8) - 20;
-
-            SystemSounds.Asterisk.Play();
-
-            notificationWindow.Show();
+            Notification a = new Notification();
         }
     }
 }
--- CustomNotification/CustomNotification/Notification/Notification.cs
+++ CustomNotification/CustomNotification/Notification/Notification.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace CustomNotification
 {
-    class Notification
+    public class Notification
     {
+        public Notification()
+        {
+            NotificationWindow notificationWindow = new NotificationWindow();
+
+            notificationWindow.Width = SystemParameters.PrimaryScreenWidth / 4;
+            notificationWindow.Height = SystemParameters.PrimaryScreenHeight / 8;
+
+            notificationWindow.Left = SystemParameters.PrimaryScreenWidth - (SystemParameters.PrimaryScreenWidth / 4) - 20;
+            notificationWindow.Top = SystemParameters.PrimaryScreenHeight - (SystemParameters.PrimaryScreenHeight / 8) - 20;
+
+            SystemSounds.Asterisk.Play();
+
+            notificationWindow.Show();
+        }
     }
 }
